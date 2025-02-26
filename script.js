0e3cd2a370709edcbcee1c5ef3decaa816e3df4c
saldanha0107 <161604967+saldanha0107@users.noreply.github.com>
2025-02-26 12:15:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.styles.fontSize = `${tamanhoAtualFonte}rem`
 
     })
 
     diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.styles.fontSize = `${tamanhoAtualFonte}rem`
 
     })
 })
